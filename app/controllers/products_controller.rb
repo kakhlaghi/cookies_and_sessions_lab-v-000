fe9f33068710744bfe99ef6e630961d0017c2e4b
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-10-30 16:13:01 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,11 +4,7 @@ class ProductsController < ApplicationController
   end
 
   def add
-    # Get the item from the path
-    @product = Product.find(params[:id])
- 
-   # Load the cart from the session, or create a new empty cart.
-    current_cart << @product.id
+    
   end
   
 end
